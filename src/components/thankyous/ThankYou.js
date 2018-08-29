67b06fa74af021424ac7c7e36fb0851f505cc2ca
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-08-29 17:44:02 +0100

    started on thank yous

--- src/components/thankyous/ThankYou.js
+++ src/components/thankyous/ThankYou.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Auth from '../../lib/Auth';
+import { Carousel } from 'react-bootstrap';
 
 
 class ThankYou extends React.Component {
@@ -19,10 +20,47 @@ class ThankYou extends React.Component {
 
           <div className="celebrations">
 
-            <h2>Thank you NAME</h2>
+            <h2>Thank you</h2>
             <h3 className="subhead">We had such a lovely day and we are so happy you could share it with us!</h3>
-            <p>Photo of Jav and Char from wedding</p>
-            <h4 className="message">UNIQUE MESSAGE</h4>
+            <div className="us">
+              <Carousel>
+                <Carousel.Item>
+                  <div className="carousel">
+                    <img className="carousel-images" alt="Our wedding" src="../assets/images/wed1.png" />
+                  </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <div className="carousel">
+                    <img className="carousel-images" alt="Our wedding" src="../assets/images/wed2.jpg" />
+                  </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <div className="carousel">
+                    <img className="carousel-images" alt="Our wedding" src="../assets/images/wed3.jpg" />
+                  </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <div className="carousel">
+                    <img className="carousel-images" alt="Our wedding" src="../assets/images/wed4.png" />
+                  </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <div className="carousel">
+                    <img className="carousel-images" alt="Our wedding" src="../assets/images/wed5.png" />
+                  </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <div className="carousel">
+                    <img className="carousel-images" alt="Our wedding" src="../assets/images/wed6.png" />
+                  </div>
+                </Carousel.Item>
+              </Carousel>
+            <h4 className="message">We were overwhelmed by the love and kindness from you all. Thank you so much for being there. We hope you enjoyed the day as much as we did! Proper thank yous to follow but in the meantime please know how much we love you all and appreciate your support.</h4>
 
             <h4 className="lotsoflove">Lots of love from Jav and Char xxx</h4>
           </div>}
